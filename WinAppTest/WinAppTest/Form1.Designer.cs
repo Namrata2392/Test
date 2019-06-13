245b71cd66bfa1ec6d07b4d154968b35e9da33b9
Namrata2392 <namrata2392@gmail.com>
2019-06-13 11:54:09 +0530

    WinApp Commit

---
+++ WinAppTest/WinAppTest/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WinAppTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.txtNo1 = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtNo2 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDev = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(12, 14);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(59, 15);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Number1";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(12, 38);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(59, 15);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "Number2";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(12, 62);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(43, 15);
+            this.lblOutput.TabIndex = 2;
+            this.lblOutput.Text = "Output";
+            // 
+            // txtNo1
+            // 
+            this.txtNo1.Location = new System.Drawing.Point(78, 13);
+            this.txtNo1.Name = "txtNo1";
+            this.txtNo1.Size = new System.Drawing.Size(299, 20);
+            this.txtNo1.TabIndex = 3;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(78, 61);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(299, 20);
+            this.txtOutput.TabIndex = 4;
+            // 
+            // txtNo2
+            // 
+            this.txtNo2.Location = new System.Drawing.Point(78, 37);
+            this.txtNo2.Name = "txtNo2";
+            this.txtNo2.Size = new System.Drawing.Size(299, 20);
+            this.txtNo2.TabIndex = 5;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(78, 87);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(55, 27);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSub.Location = new System.Drawing.Point(139, 87);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(55, 27);
+            this.btnSub.TabIndex = 7;
+            this.btnSub.Text = "SUB";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMul.Location = new System.Drawing.Point(200, 87);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(55, 27);
+            this.btnMul.TabIndex = 8;
+            this.btnMul.Text = "MULTI";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
+            // 
+            // btnDev
+            // 
+            this.btnDev.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDev.Location = new System.Drawing.Point(261, 87);
+            this.btnDev.Name = "btnDev";
+            this.btnDev.Size = new System.Drawing.Size(55, 27);
+            this.btnDev.TabIndex = 9;
+            this.btnDev.Text = "DIV";
+            this.btnDev.UseVisualStyleBackColor = true;
+            this.btnDev.Click += new System.EventHandler(this.btnDev_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(322, 87);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(55, 27);
+            this.btnClose.TabIndex = 10;
+            this.btnClose.Text = "CLOSE";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 139);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDev);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtNo2);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtNo1);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Arithmatic Operations";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox txtNo1;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtNo2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDev;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ WinAppTest/WinAppTest/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAppTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int Num = int.Parse(txtNo1.Text);
+            int Num2 = int.Parse(txtNo2.Text);
+            int Sum = Num + Num2;
+            txtOutput.Text = Sum.ToString();
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            int Num = int.Parse(txtNo1.Text);
+            int Num2 = int.Parse(txtNo2.Text);
+            int Sum = Num - Num2;
+            txtOutput.Text = Sum.ToString();
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            int Num = int.Parse(txtNo1.Text);
+            int Num2 = int.Parse(txtNo2.Text);
+            int Sum = Num * Num2;
+            txtOutput.Text = Sum.ToString();
+        }
+
+        private void btnDev_Click(object sender, EventArgs e)
+        {
+            int Num = int.Parse(txtNo1.Text);
+            int Num2 = int.Parse(txtNo2.Text);
+            int Sum = Num % Num2;
+            txtOutput.Text = Sum.ToString();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
